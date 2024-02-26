2d1e715e4dae72f1a1138acf0c7feb3c64ee1f3e
JestkieDev <yongeryongerov@gmail.com>
2024-02-26 21:54:11 +0400

    Небольшие правки

--- app/src/main/java/com/example/medcalc/MainActivity.java
+++ app/src/main/java/com/example/medcalc/MainActivity.java
@@ -8,6 +8,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
